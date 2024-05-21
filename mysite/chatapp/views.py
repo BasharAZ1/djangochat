8fa519f798d85385aa91ea068401a0c8c96e01f8
basharaz1 <basharalli@h-MacBook-Pro-sl-Bashar.local>
2024-05-21 14:06:55 +0300

    add requirements

--- mysite/chatapp/views.py
+++ mysite/chatapp/views.py
@@ -27,7 +27,7 @@ def test(request):
     chat_type='personal'
 )
     
-    # If such a room exists, get its messages
+   
     if chat_rooms_for_both_users.exists():
         chatroom = chat_rooms_for_both_users.first()
         messages = ChatMessage.objects.filter(room=chatroom)[0:30]
@@ -56,17 +56,17 @@ def create_privateroom(request):
 
     for chat_room in chat_rooms_for_user1:
         if len(chat_room.users.filter(id=user2.id))>0:
-            #chatroom = ChatRoom.objects.get(slug=chat_room.slug)
+       
             messages = ChatMessage.objects.filter(room=chatroom)[0:30]
             return render(request,'chatapp/room.html',{'chatroom':chat_room,'messages':messages})
 
-    # If the chat room doesn't exist, create a new one
+ 
     new_chatroom = ChatRoom.objects.create(slug=slug,name=name)
     new_chatroom.users.add(user1)
     new_chatroom.users.add(user2)
     new_chatroom.save()
 
-    # Redirect to the newly created chat room
+
     return render(request,'chatapp/room.html',{'chatroom':new_chatroom,'messages':''})
 
 
@@ -74,24 +74,21 @@ def create_group_chatroom(request):
     if request.method == 'POST':
         user_ids = request.POST.getlist('user_ids')
         groupName=request.POST('groupName')
-        # Create a new chat room
         new_chatroom = ChatRoom.objects.create(slug=groupName)
-        # Add the users to the chat room
+  
         for user_id in user_ids:
             try:
                 user = User.objects.get(id=user_id)
                 new_chatroom.users.add(user)
             except User.DoesNotExist:
-                # Handle the case where a user does not exist
-                new_chatroom.delete()  # Delete the chat room if a user is not found
+                
+                new_chatroom.delete() 
                 return JsonResponse({"error": f"User with ID {user_id} does not exist."}, status=404)
 
-        # Redirect to the newly created chat room
-        return JsonResponse({"message": "Group chat room created.", "slug": new_chatroom.slug})
 
-    # Handle GET request (render a form to create a group chat)
+        return JsonResponse({"message": "Group chat room created.", "slug": new_chatroom.slug})
     return JsonResponse({"message": "Chat room created.", "slug": new_chatroom.slug})
 def delete_chatroom(request, slug):
     chat_room = get_object_or_404(ChatRoom, slug=slug)
     chat_room.delete()
-    return HttpResponse("chatRoom Deleted")  # Replace 'chatroom_list' with the appropriate UmeRL na
+    return HttpResponse("chatRoom Deleted")  
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -6,10 +6,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-=g=s12=+lgp=!)$pxc!n!c39zz^w-&bzma4y854w_($z@5updr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
